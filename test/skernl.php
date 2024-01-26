ed90e50f7aba2621e854bb7b5f0eba72ed29d720
wheakerd <wheakerd@qq.com>
2024-01-26 15:10:48 +0800

    测试堆管理

--- test/skernl.php
+++ test/skernl.php
@@ -1,33 +1,59 @@
 #! /usr/bin/php
 <?php
 
-//$memoryTable = new Swoole\Table(1024);
-// 记录脚本开始时的内存使用情况
-use Skernl\Di\ClassesManager;
+class A
+{
+    public function get()
+    {
+        return 1;
+    }
+}
 
-$startMemory = memory_get_usage();
-$startTime = microtime(true);
+$table = new Swoole\Table(1024);
+$table->column('name', Swoole\Table::TYPE_STRING, 64);
+$table->create();
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+$obj = new A();
 
-$loaders = Composer\Autoload\ClassLoader::getRegisteredLoaders();
-$loaders = reset($loaders);
-$classMap = $loaders->getClassMap();
+$a = serialize($obj);
 
-/** @noinspection PhpUnhandledExceptionInspection */
-$manager = new ClassesManager($classMap);
+$table->set('1', ['name' => $a]);
 
-//$indexController = $manager->getContainer()->get(\App\Controller\Index::class);
-//var_dump((new \App\Controller\IndexController($indexController))->getDefaultValue());
+$objA = $table->get('1', 'name');
 
-// 记录脚本结束时的内存使用情况
-$endMemory = memory_get_usage();
-$endTime = microtime(true);
-// 计算内存使用量
-$memoryUsed = $endMemory - $startMemory;
-$time = $endTime - $startTime;
+$classA = unserialize($objA);
 
-// 输出内存使用情况
-echo "Time used: " . $time . PHP_EOL;
-echo "Memory used: " . $memoryUsed . PHP_EOL;
-echo "Peak memory usage: " . memory_get_peak_usage() . PHP_EOL;
+var_dump($classA->get());
+
+
+//
+////$memoryTable = new Swoole\Table(1024);
+//// 记录脚本开始时的内存使用情况
+//use Skernl\Di\ClassesManager;
+//
+//$startMemory = memory_get_usage();
+//$startTime = microtime(true);
+//
+//require dirname(__DIR__) . '/vendor/autoload.php';
+//
+//$loaders = Composer\Autoload\ClassLoader::getRegisteredLoaders();
+//$loaders = reset($loaders);
+//$classMap = $loaders->getClassMap();
+//
+///** @noinspection PhpUnhandledExceptionInspection */
+//$manager = new ClassesManager($classMap);
+//
+////$indexController = $manager->getContainer()->get(\App\Controller\Index::class);
+////var_dump((new \App\Controller\IndexController($indexController))->getDefaultValue());
+//
+//// 记录脚本结束时的内存使用情况
+//$endMemory = memory_get_usage();
+//$endTime = microtime(true);
+//// 计算内存使用量
+//$memoryUsed = $endMemory - $startMemory;
+//$time = $endTime - $startTime;
+//
+//// 输出内存使用情况
+//echo "Time used: " . $time . PHP_EOL;
+//echo "Memory used: " . $memoryUsed . PHP_EOL;
+//echo "Peak memory usage: " . memory_get_peak_usage() . PHP_EOL;
